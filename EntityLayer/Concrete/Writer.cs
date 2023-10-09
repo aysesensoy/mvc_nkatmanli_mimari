d80f0be3219fb37a42d46eb9ed04b5fed70e7aed
Ayşe Şensoy <51344094+aysesensoy@users.noreply.github.com>
2023-10-09 20:14:15 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        public int WriterID { get; set; }
+        public string UserName { get; set; }
+        public string WriterSurname { get; set; }
+        public string WriterMail { get; set; }
+        public string WriterPassword { get; set;}
+
+
+    }
+}
